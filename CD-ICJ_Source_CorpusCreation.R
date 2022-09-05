ae3e0519a054eec184bc2a28a4293ce02c9b84d8
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-09-05 20:17:51 +0200

    add author

--- CD-ICJ_Source_CorpusCreation.R
+++ CD-ICJ_Source_CorpusCreation.R
@@ -1,6 +1,6 @@
 #'---
 #'title: "Compilation Report | Corpus of Decisions: International Court of Justice (CD-ICJ)"
-#'author: Anonymized for Peer Review
+#'author: Seán Fobbe
 #'geometry: margin=3cm
 #'papersize: a4
 #'fontsize: 11pt
